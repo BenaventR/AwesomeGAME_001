05871ce1670babeb0ed52496b2fc470f57ed820b
Ruinanw <ruinanw97@gmail.com>
2024-11-09 20:24:43 -0800

    art

--- Ruinanw/char_movement.gd
+++ Ruinanw/char_movement.gd
@@ -5,6 +5,7 @@ const SPEED = 400.0
 const JUMP_VELOCITY = -500.0
 var jumpnum =0
 var maxjump =2
+var shootdir = 1
 signal shoot
 
 var screen_size
@@ -25,6 +26,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	print(direction)
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -33,9 +35,14 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 	
 	
+	if direction==1:
+		shootdir = 1
+	elif direction==-1:
+		shootdir = -1
+	
 	var pos = get_node("../playa").global_position
-	var dir = Vector2(direction,0)
+	var sdir = Vector2(shootdir,0)
 	if Input.is_action_just_pressed("shoot"):
-		shoot.emit(pos,dir)
+		shoot.emit(pos,sdir)
 		#print(direction)
 		
